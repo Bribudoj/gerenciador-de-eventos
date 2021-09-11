ecdc81d27e1eb77e6a628bcedf16a8ed540a04f0
Bribudoj <eduardomsantana@outlook.com.br>
2021-09-10 21:07:53 -0300

    Init

---
+++ cadastrar-eventos/src/main/java/com/seduardo/cadastrareventos/CadastrarEventosApplication.java
@@ -0,0 +1,13 @@
+package com.seduardo.cadastrareventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrarEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrarEventosApplication.class, args);
+	}
+
+}
